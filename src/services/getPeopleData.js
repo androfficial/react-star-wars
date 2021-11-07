122c45f29aa97910f60663c3a25d8e8f7679fb2c
androfficial <material1fox@gmail.com>
2021-11-07 21:21:38 +0200

    Added -services-

---
+++ src/services/getPeopleData.js
@@ -0,0 +1,25 @@
+import { Constants } from '../constants';
+
+// Проверка протокола: HTTP или HTTPS
+const checkProtocol = (url) => {
+  if (url.indexOf(Constants.HTTPS) !== -1) {
+    return Constants.HTTPS;
+  }
+
+  return Constants.HTTP;
+};
+
+// Получить ID персонажа с URL
+const getId = (url, category) => {
+  const protocol = checkProtocol(url);
+
+  const id = url.replace(protocol + Constants.SWAPI_ROOT + category, '').replace(/\//g, '');
+
+  return id;
+};
+
+export const getPeopleId = (url) => getId(url, Constants.SWAPI_PEOPLE);
+
+// Получить изображение для персонажа
+export const getPeopleImage = (id) =>
+  `${Constants.URL_IMG_PERSON}/${id + Constants.GUIDE_IMG_EXTENSION}`;
