b91ea6fa45e6b6c36ff250cb01a91df259a14f65
androfficial <material1fox@gmail.com>
2021-11-07 21:19:17 +0200

    Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const Constants = {
+  HTTP: 'http://',
+  HTTPS: 'https://',
+  SWAPI_ROOT: 'swapi.dev/api/',
+  SWAPI_PEOPLE: 'people',
+  URL_IMG_PERSON: 'https://starwars-visualguide.com/assets/img/characters',
+  GUIDE_IMG_EXTENSION: '.jpg',
+};
