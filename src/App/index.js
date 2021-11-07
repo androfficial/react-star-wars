0ef51dbe404814bbd2c19cb2f5c8c481c94d641f
androfficial <material1fox@gmail.com>
2021-11-07 12:59:47 +0200

    Renaming to modular css

---
+++ src/App/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import cn from 'classnames';
+import s from '../styles/index.module.css';
+
+const App = () => {
+  return <div className={cn(s.header)}>Дарова</div>;
+};
+
+export default App;
