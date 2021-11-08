30f637d55618c1550f42d24ec22a0329d9bb3515
androfficial <material1fox@gmail.com>
2021-11-08 13:06:17 +0200

    Updated page -People-

--- src/pages/People/index.jsx
+++ src/pages/People/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import s from '../../styles/style.module.scss';
+
 const People = ({ people }) => {
   return (
-    <ul className="list">
-      {people.map(({ id, name, img }) => (
-        <li key={`${id}_${name}`} className="item">
-          <img src={img} alt={name} />
-          <p className="name">{name}</p>
-        </li>
-      ))}
-    </ul>
+    <div className={s.people}>
+      <ul className={s.people_list}>
+        {people && people.map(({ id, name: personName, img }) => (
+          <li key={`${id}_${personName}`} className={s.people_item}>
+            <a href="/#" className={s.people_link}>
+              <img src={img} alt={personName} />
+              <p className={s.people_name}>{personName}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
