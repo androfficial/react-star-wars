0f9e481ecfe92e79e1664715180231240eb8f797
androfficial <material1fox@gmail.com>
2021-11-18 21:48:17 +0200

    Added preloader to input on Search page

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -15,11 +15,13 @@ import s from '@styles/style.module.scss';
 const Search = () => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [inProgress, setInProgress] = useState(false);
   const [value, setValue] = useState('');
   const [people, setPeople] = useState([]);
 
   const getSearchApi = useCallback(
     async (value) => {
+      setInProgress(true);
       const res = await mainAPI.getSearchData(value);
       if (res) {
         const resPeople = res.data.results;
@@ -33,10 +35,12 @@ const Search = () => {
             image: img,
           };
         });
+        setInProgress(false);
         setPeople(resFilteredPeople);
 
         dispatch(setErrorApi(false));
       } else {
+        setInProgress(false);
         dispatch(setErrorApi(true));
       }
     },
@@ -58,16 +62,15 @@ const Search = () => {
 
   const handleInputChange = (e) => {
     const value = e.target.value;
-
     setValue(value);
+
     debounceGetResp(value);
   };
 
-  const handleInputClear = async () => {
+  const handleInputClear = () => {
     setValue('');
-    setIsLoaded(false);
-    await getSearchApi('');
-    setIsLoaded(true);
+
+    getSearchApi('');
   };
 
   return (
@@ -83,20 +86,40 @@ const Search = () => {
               className={s.search_input}
               placeholder="Input character's name"
             />
-            <svg
-              onClick={handleInputClear}
-              className={cn(s.search_svg, !value && s.disabled)}
-              viewBox="0 0 492 492">
-              <path
-                d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872
+            {inProgress ? (
+              <svg
+                className={s.search_in_progress}
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                style={{ shapeRendering: 'auto' }}
+                viewBox="0 0 100 100"
+                preserveAspectRatio="xMidYMid">
+                <path d="M10 50A40 40 0 0 0 90 50A40 45 0 0 1 10 50" fill="#808080" stroke="none">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    dur="1s"
+                    repeatCount="indefinite"
+                    keyTimes="0;1"
+                    values="0 50 52.5;360 50 52.5"></animateTransform>
+                </path>
+              </svg>
+            ) : (
+              <svg
+                onClick={handleInputClear}
+                className={cn(s.search_svg, !value && s.disabled)}
+                viewBox="0 0 492 492">
+                <path
+                  d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872
               c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872
               c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052
               L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116
               c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952
               c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116
               c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"
-              />
-            </svg>
+                />
+              </svg>
+            )}
           </div>
           {people.length > 0 ? (
             <ul className={s.search_list}>
