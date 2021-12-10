7a494b64a427e3766ad887f08579b56c3b1828c2
androfficial <material1fox@gmail.com>
2021-12-10 21:18:44 +0200

    Changes: - Added linting of style files - Config-overrides.js updated, now styles linting is live - Added .stylelintrc.json config - Added .stylelintignore config - Minor edits in .eslintrc.json config - Updated package.json dependencies - Updated yarn.lock file

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,32 @@
+const path = require('path');
 const { alias } = require('react-app-rewire-alias');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-module.exports = function override(config, env) {
-  alias({
-    '@assets': 'src/assets',
-    '@components': 'src/components',
-    '@constants': 'src/constants',
-    '@context': 'src/context',
-    '@hoc': 'src/hoc',
-    '@hooks': 'src/hooks',
-    '@pages': 'src/pages',
-    '@redux': 'src/redux',
-    '@services': 'src/services',
-    '@styles': 'src/styles',
-    '@utils': 'src/utils',
-  })(config);
+module.exports = {
+  webpack(config, env) {
+    if (env === 'development') {
+      config.plugins.push(
+        new StyleLintPlugin({
+          configFile: path.resolve(__dirname, '.stylelintrc.json'),
+          files: path.resolve(__dirname, 'src/styles/**/*.{sass,scss}'),
+          failOnError: false,
+        })
+      );
+      alias({
+        '@assets': 'src/assets',
+        '@components': 'src/components',
+        '@constants': 'src/constants',
+        '@context': 'src/context',
+        '@hoc': 'src/hoc',
+        '@hooks': 'src/hooks',
+        '@pages': 'src/pages',
+        '@redux': 'src/redux',
+        '@services': 'src/services',
+        '@styles': 'src/styles',
+        '@utils': 'src/utils',
+      })(config);
+    }
 
-  return config;
+    return config;
+  },
 };
